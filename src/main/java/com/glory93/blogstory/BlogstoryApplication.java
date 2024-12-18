4dd209194a62e2e73bbf353a5c741a8ed0725a05
glory9312 <1210puppup@gmail.com>
2024-12-18 16:26:31 +0900

    Initial commit

---
+++ src/main/java/com/glory93/blogstory/BlogstoryApplication.java
@@ -0,0 +1,13 @@
+package com.glory93.blogstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glory93/blogstory/BlogstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glory93.blogstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
